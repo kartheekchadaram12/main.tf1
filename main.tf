ca17a1780acd6ecc4475a9662a44617fb395c3c5
kartheekchadaram12 <kartheekchadaram12@gmail.com>
2024-10-04 15:14:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource  "aws_instance"  "one"{
 
   name           = "my-ec2-instance"
   instance_type  = "t2.micro"
-  key_name       = "your-key-pair"  # Replace with your key pair
   ami            = "ami-0c55b159cbfafe1f0"  # Replace with an appropriate AMI ID for your region
 
   tags = {
