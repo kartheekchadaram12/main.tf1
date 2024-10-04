d92aa1b5f2ae3404c2ee4e75484e417046a95bb5
kartheekchadaram12 <kartheekchadaram12@gmail.com>
2024-10-04 15:15:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ provider "aws" {
 
 resource  "aws_instance"  "one"{
  
-
-  name           = "my-ec2-instance"
   instance_type  = "t2.micro"
   ami            = "ami-0c55b159cbfafe1f0"  # Replace with an appropriate AMI ID for your region
 
